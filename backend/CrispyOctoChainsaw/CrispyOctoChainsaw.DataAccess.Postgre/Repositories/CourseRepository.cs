90a8ee963e8107414b925d6176a47ce17e56fe15
mayr0y <alexlzrw@gmail.com>
2022-09-03 20:01:36 +0300

    fix: fixed Cource

---
+++ backend/CrispyOctoChainsaw/CrispyOctoChainsaw.DataAccess.Postgre/Repositories/CourseRepository.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using CrispyOctoChainsaw.DataAccess.Postgres.Entities;
+using CrispyOctoChainsaw.Domain.Interfaces;
+using CrispyOctoChainsaw.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrispyOctoChainsaw.DataAccess.Postgres.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly CrispyOctoChainsawDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CourseRepository(CrispyOctoChainsawDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<Course[]> Get()
+        {
+            var courses = await _context.Courses.AsNoTracking().ToArrayAsync();
+            return _mapper.Map<CourseEntity[], Course[]>(courses);
+        }
+
+        public async Task<Course?> Get(long courseId)
+        {
+            var course = await _context.Courses.AsNoTracking().FirstOrDefaultAsync(x => x.Id == courseId);
+            if (course is null)
+            {
+                return null;
+            }
+            return _mapper.Map<CourseEntity, Course>(course);
+        }
+    }
+}
---
+++ backend/CrispyOctoChainsaw/CrispyOctoChainsaw.Domain/Interfaces/ICourseRepository.cs
@@ -0,0 +1,10 @@
+﻿using CrispyOctoChainsaw.Domain.Model;
+
+namespace CrispyOctoChainsaw.Domain.Interfaces
+{
+    public interface ICourseRepository
+    {
+        Task<Course[]> Get();
+        Task<Course?> Get(long courseId);
+    }
+}
