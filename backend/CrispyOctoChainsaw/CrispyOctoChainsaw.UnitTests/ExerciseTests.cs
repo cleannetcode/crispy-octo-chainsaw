cd208dd87c0366556f0ae21096aa5d325310e691
IlyaEliseev <84148446+IlyaEliseev@users.noreply.github.com>
2022-11-26 19:09:54 +0300

    add exercise unit tests

---
+++ backend/CrispyOctoChainsaw/CrispyOctoChainsaw.UnitTests/ExerciseTests.cs
@@ -0,0 +1,58 @@
+﻿using AutoFixture;
+using CrispyOctoChainsaw.Domain.Model;
+using CrispyOctoChainsaw.UnitTests.MemberData;
+
+namespace CrispyOctoChainsaw.UnitTests
+{
+    public class ExerciseTests
+    {
+        private readonly Fixture _fixture;
+
+        public ExerciseTests()
+        {
+            _fixture = new Fixture();
+        }
+
+        [Fact]
+        public async Task Create_ShouldReturnNewExercise()
+        {
+            // arrange
+            var title = _fixture.Create<string>();
+            var description = _fixture.Create<string>();
+            var branchName = _fixture.Create<string>();
+            var courseId = _fixture.Create<int>();
+
+            // act
+            var result = Exercise.Create(title, description, branchName, courseId);
+
+            // assert
+            Assert.NotNull(result.Value);
+            Assert.False(result.IsFailure);
+            Assert.Equal(title, result.Value.Title);
+            Assert.Equal(description, result.Value.Description);
+            Assert.Equal(branchName, result.Value.BranchName);
+            Assert.Equal(courseId, result.Value.CourseId);
+        }
+
+        [Theory]
+        [MemberData(
+            nameof(ExerciseDataGenerator.GenerateSetInvalidAllParameters),
+            parameters: 10,
+            MemberType = typeof(ExerciseDataGenerator))]
+        public void Create_AllParametersIsInvalid_ShouldReturnErrors(
+            string title,
+            string description,
+            string branchName,
+            int courseId)
+        {
+            // arrange
+            // act
+            var result = Exercise.Create(title, description, branchName, courseId);
+
+            // assert
+            Assert.False(result.IsSuccess);
+            Assert.True(result.IsFailure);
+            Assert.True(result.Error.Length != 0);
+        }
+    }
+}
---
+++ backend/CrispyOctoChainsaw/CrispyOctoChainsaw.UnitTests/MemberData/ExerciseDataGenerator.cs
@@ -0,0 +1,57 @@
+﻿using CrispyOctoChainsaw.Domain.Model;
+
+namespace CrispyOctoChainsaw.UnitTests.MemberData
+{
+    public class ExerciseDataGenerator
+    {
+        public static IEnumerable<object[]> GenerateSetInvalidAllParameters(int testCount)
+        {
+            var rnd = new Random();
+            for (int i = 0; i < testCount; i++)
+            {
+                // title
+                var titleInvalidLength = rnd.Next(
+                    Exercise.MaxTitleLength,
+                    Exercise.MaxTitleLength + 4);
+
+                var invalidTitleData = Enumerable.Range(0, 5)
+                    .Select(x => StringFixture.GenerateRandomString(titleInvalidLength))
+                    .ToArray();
+
+                var invalidTitle = BaseDataGenerator.MakeInvalidString(invalidTitleData);
+
+                // description
+                var descriptionInvalidLength = rnd.Next(
+                    Exercise.MaxDescriptionsLength,
+                    Exercise.MaxDescriptionsLength + 4);
+
+                var invalidDescriptionData = Enumerable.Range(0, 5)
+                    .Select(x => StringFixture.GenerateRandomString(descriptionInvalidLength))
+                    .ToArray();
+
+                var invalidDescription = BaseDataGenerator.MakeInvalidString(invalidDescriptionData);
+
+                // branchName
+                var branchyNameInvalidLength = rnd.Next(
+                    Exercise.MaxBranchNameLength,
+                    Exercise.MaxBranchNameLength + 4);
+
+                var invalidBranchNameData = Enumerable.Range(0, 5)
+                    .Select(x => StringFixture.GenerateRandomString(branchyNameInvalidLength))
+                    .ToArray();
+
+                var invalidBranchName = BaseDataGenerator.MakeInvalidString(invalidBranchNameData);
+
+                var invalidId = BaseDataGenerator.MakeInvalidId();
+
+                yield return new object[]
+                {
+                    invalidTitle,
+                    invalidDescription,
+                    invalidBranchName,
+                    invalidId
+                };
+            }
+        }
+    }
+}
